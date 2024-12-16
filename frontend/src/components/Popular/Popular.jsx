460f394996f77cc4502be283bab94330fa64af7b
Arbaz Ahmad <146482270+Arbaz-Ahmad95@users.noreply.github.com>
2024-12-16 21:08:37 +0530

    Update Popular.jsx

--- frontend/src/components/Popular/Popular.jsx
+++ frontend/src/components/Popular/Popular.jsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import './popular.css'
+import data_product from '../Assets/data'
 
 import {Link} from 'react-router-dom'
 
 const Popular = () => {
 
 
-        const [popularProducts,setPopularProducts]=useState([])
-
-        useEffect(()=>{
-            fetch(`${process.env.REACT_APP_BACKEND_URL}/popularinwomen`)
-            .then((response)=>response.json())
-            .then((data)=>setPopularProducts(data))
-
-        },[])
+       
     
     return (
         <div className='popular'>
             <h1>POPULAR IN WOMEN</h1>
             <hr />
             <div className="popular-item">
-                {popularProducts.map((item) => (
+                {data_product.map((item) => (
                     <div key={item.id} className="product-card">
                         <Link to ={`/product/${item.id}`}><img src={item.image} alt={item.name} className="product-image" /> </Link>
                         <p className="product-name">{item.name}</p>
